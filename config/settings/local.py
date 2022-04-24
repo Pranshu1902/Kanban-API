ec36e1e994700b80692946ebddfdbd826a920fc4
Pranshu1902 <pranshu1902@gmail.com>
2022-04-25 01:30:09 +0530

    test

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,7 +12,7 @@ SECRET_KEY = env(
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["kanban-api-pranshu1902.herokuapp.com", "localhost", "0.0.0.0", "127.0.0.1"]
-
+#
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
